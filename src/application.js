8e0547c889d9823f75febfe8a075bdaa256a4f08
RVelibegov <velibegov@mail.ru>
2022-10-25 18:28:10 +0300

    add modal preview

--- src/application.js
+++ src/application.js
@@ -1,130 +1,146 @@
-import {watchState} from './watchers.js';
-import {object, string} from 'yup';
+import { object, string, setLocale } from 'yup';
 import i18next from 'i18next';
-import ru from "./locales/ru.json";
-import {setLocale} from 'yup';
+import ru from './locales/ru.json';
+import watchState from './watchers.js';
 
 const app = () => {
+  const resources = {
+    ru: {
+      translation: ru,
+    },
+  };
 
-    const resources = {
-        ru: {
-            translation: ru
-        }
-    };
+  i18next.init({
+    lng: 'ru',
+    resources,
+  });
 
-    i18next.init({
-        lng: "ru",
-        // debug: true,
-        resources
+  setLocale({
+    mixed: {
+      notOneOf: i18next.t(ru.rssForm.feedback.urlAlreadyExists),
+    },
+    string: {
+      url: i18next.t(ru.rssForm.feedback.notValidUrl),
+    },
+  });
+
+  const state = {
+    rssForm: {
+      isValid: false,
+      feedback: '',
+      error: '',
+      uniqAttribute: 0,
+      modalContent: {},
+    },
+    urls: [],
+    viewedUrls: [],
+    feeds: [],
+  };
+
+  const schema = object({
+    url: string()
+      .url()
+      .required()
+      .notOneOf([state.urls]),
+  });
+
+  const rssParse = (content) => new DOMParser().parseFromString(content, 'text/xml');
+
+  const urlsBypass = (urls) => {
+    const promises = [];
+    Array.prototype.forEach.call(urls, (url) => {
+      promises.push(
+        fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url.toString())}`)
+          .then((response) => response.json()),
+      );
     });
+    return promises;
+  };
 
-    setLocale({
-        mixed: {
-            notOneOf: i18next.t(ru.rssForm.feedback.urlAlreadyExists),
-        },
-        string: {
-            url: i18next.t(ru.rssForm.feedback.notValidUrl),
-        },
+  const addPreviewListeners = () => {
+    const elements = document.querySelectorAll('button[data-id]');
+    elements.forEach((element) => {
+      element.addEventListener('click', () => {
+        const id = parseInt(element.dataset.id, 10);
+        const viewed = document.querySelector(`a[data-id="${id}"]`);
+        let content = [];
+        state.feeds.map((feed) => content = feed.items.filter((item) => item.dataId === id));
+        state.rssForm.modalContent = {
+          title: content[0].title,
+          description: content[0].description.replace('<![CDATA[', '').replace(']]>', ''),
+          link: content[0].link,
+        };
+        watchState(state).viewedUrls.push(viewed.href);
+      });
     });
+  };
 
-    const state = {
-        rssForm: {
-            isValid: false,
-            feedback: '',
-            error: '',
-        },
-        urls: [],
-        viewedUrls: [],
-        feeds: [],
-    };
-
-    const schema = object({
-        url: string()
-            .url()
-            .required()
-            .notOneOf([state.urls])
-    });
-
-    const rssParse = (content) => {
-        return new DOMParser().parseFromString(content, "text/xml");
-    };
-
-    const urlsByPass = (urls) => {
-        const promises = [];
-        Array.prototype.forEach.call(urls, url => {
-            promises.push(
-                fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url.toString())}`)
-                    .then(response => {
-                        if (response.ok) return response.json();
-                    })
-            )
-        });
-        return promises;
-    };
-
-    const feedsUpdate = (promises) => {
-        Promise.all(promises)
-            .then((data) => {
-                data.forEach((item) => {
-                    const content = rssParse(item.contents);
-                    if (!content.querySelector('parsererror')) {
-                        const itemList = [];
-                        const items = content.querySelectorAll('item');
-                        items.forEach((item) => {
-                            itemList.push({
-                                'title': item.querySelector('title').innerHTML,
-                                'description': item.querySelector('description').innerHTML,
-                                'link': item.querySelector('link').innerHTML,
-                            });
-                        });
-                        state.rssForm.feedback = i18next.t(ru.rssForm.feedback.success);
-                        const currentLink = content.querySelector('link').innerHTML;
-                        const links = [];
-                        state.feeds.forEach((feed) => {
-                            links.push(feed.link);
-                        });
-                        if (!links.includes(currentLink)) {
-                            watchState(state).feeds.push({
-                                'link': currentLink,
-                                'title': content.querySelector('title').innerHTML,
-                                'description': content.querySelector('description').innerHTML,
-                                'items': itemList,
-                            });
-                        }
-                    } else {
-                        watchState(state).rssForm.error = i18next.t(ru.rssForm.feedback.notValidRss);
-                    }
-
-                });
-            })
-            .catch(() => {
-                watchState(state).rssForm.error = i18next.t(ru.rssForm.feedback.networkProblems);
-                watchState(state).rssForm.isValid = false;
-            })
-    };
-
-    const form = document.getElementsByClassName('rss-form').item(0);
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const data = new FormData(form);
-        const url = data.get('url');
-        schema.validate({url})
-            .then(() => {
-                state.urls.push(url);
-                state.rssForm.isValid = true;
-                feedsUpdate(urlsByPass(state.urls));
-            })
-            .catch((error) => {
-                watchState(state).rssForm.error = error.message;
-                watchState(state).rssForm.isValid = false;
-            })
-            .finally(() => {
-                 let update = setTimeout(function recursion() {
-                     feedsUpdate(urlsByPass(state.urls));
-                     update = setTimeout(recursion, 5000);
-                 }, 5000);
+  const feedsUpdate = (promises) => {
+    Promise.all(promises)
+      .then((data) => {
+        data.forEach((item) => {
+          const content = rssParse(item.contents);
+          if (!content.querySelector('parsererror')) {
+            const itemList = [];
+            const items = content.querySelectorAll('item');
+            items.forEach((current) => {
+              itemList.push({
+                title: current.querySelector('title').innerHTML,
+                description: current.querySelector('description').innerHTML,
+                link: current.querySelector('link').innerHTML,
+                dataId: state.rssForm.uniqAttribute += 1,
+              });
             });
-    });
-}
+            state.rssForm.feedback = i18next.t(ru.rssForm.feedback.success);
+            const currentLink = content.querySelector('link').innerHTML;
+            const links = [];
+            state.feeds.forEach((feed) => {
+              links.push(feed.link);
+            });
+            if (!links.includes(currentLink)) {
+              watchState(state).feeds.push({
+                link: currentLink,
+                title: content.querySelector('title').innerHTML,
+                description: content.querySelector('description').innerHTML,
+                items: itemList,
+              });
+            }
+          } else {
+            watchState(state).rssForm.error = i18next.t(ru.rssForm.feedback.notValidRss);
+          }
+        });
+      })
+      .then(() => {
+        addPreviewListeners();
+      })
+      .catch(() => {
+        watchState(state).rssForm.error = i18next.t(ru.rssForm.feedback.networkProblems);
+        watchState(state).rssForm.isValid = false;
+      });
+  };
+
+  const form = document.getElementsByClassName('rss-form').item(0);
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+    const url = data.get('url');
+    schema.validate({ url })
+      .then(() => {
+        state.urls.push(url);
+        state.rssForm.isValid = true;
+        feedsUpdate(urlsBypass(state.urls));
+      })
+      .catch((error) => {
+        watchState(state).rssForm.error = error.message;
+        watchState(state).rssForm.isValid = false;
+      })
+      .finally(() => {
+        let update = setTimeout(function recursion() {
+          feedsUpdate(urlsBypass(state.urls));
+          update = setTimeout(recursion, 5000);
+        }, 5000);
+      });
+  });
+};
 
 export default app;
--- src/watchers.js
+++ src/watchers.js
@@ -1,83 +1,130 @@
-import onChange from "on-change";
+import onChange from 'on-change';
 
-export const watchState = (state) => onChange(state, (path, value, previousValue) => {
-    render(state);
-});
+const showModal = (state) => {
+  const body = document.getElementsByTagName('body').item(0);
+  body.setAttribute('style', 'overflow: hidden; padding-right: 17px;');
+  body.classList.add('modal-open');
+  const modalContainer = document.getElementById('modal');
+  modalContainer.classList.add('show');
+  modalContainer.setAttribute('style', 'display: block;');
+  modalContainer.setAttribute('aria-modal', 'true');
+  modalContainer.removeAttribute('aria-hidden');
+  const modalTitle = document.getElementsByClassName('modal-title').item(0);
+  modalTitle.innerHTML = state.rssForm.modalContent.title;
+  const modalBody = document.getElementsByClassName('modal-body').item(0);
+  modalBody.innerHTML = state.rssForm.modalContent.description;
+  const fullArticle = document.getElementsByClassName('full-article').item(0);
+  fullArticle.setAttribute('href', state.rssForm.modalContent.link);
+  const modalCloses = modalContainer.querySelectorAll('[data-bs-dismiss="modal"]');
+  modalCloses.forEach((closer) => {
+    closer.addEventListener('click', () => {
+      body.removeAttribute('style');
+      body.classList.remove('modal-open');
+      modalContainer.classList.remove('show');
+      modalContainer.removeAttribute('style');
+      modalContainer.removeAttribute('aria-modal');
+      modalContainer.setAttribute('aria-hidden', 'true');
+      state.rssForm.modalContent = {};
+    });
+  });
+};
 
 const render = (state) => {
-    const input = document.getElementById('url-input');
-    const feedback = document.getElementsByClassName('feedback').item(0);
-    if (state.rssForm.isValid) {
-        input.classList.remove('is-invalid');
-        input.value = '';
-        feedback.classList.remove('text-danger');
-        feedback.classList.add('text-success');
-        feedback.innerHTML = state.rssForm.feedback;
-        input.focus();
-    } else {
-        feedback.classList.remove('text-success');
-        feedback.classList.add('text-danger');
-        feedback.innerHTML = state.rssForm.error;
-        input.classList.add('is-invalid');
-    }
+  const input = document.getElementById('url-input');
+  const feedback = document.getElementsByClassName('feedback').item(0);
+  if (state.rssForm.isValid) {
+    input.classList.remove('is-invalid');
+    input.value = '';
+    feedback.classList.remove('text-danger');
+    feedback.classList.add('text-success');
+    feedback.innerHTML = state.rssForm.feedback;
+    input.focus();
+  } else {
+    feedback.classList.remove('text-success');
+    feedback.classList.add('text-danger');
+    feedback.innerHTML = state.rssForm.error;
+    input.classList.add('is-invalid');
+  }
 
-    const feeds = document.getElementsByClassName('feeds').item(0);
-    feeds.innerHTML = '';
-    const feedsBorder = document.createElement('div');
-    feedsBorder.classList.add('card', 'border-0');
-    const feedsBody = document.createElement('div');
-    feedsBody.classList.add('card-body');
-    const feedsTitle = document.createElement('h2');
-    feedsTitle.classList.add('card-title', 'h4');
-    feedsTitle.innerHTML = 'Фиды';
-    const feedsUl = document.createElement('ul');
-    feedsUl.classList.add('list-group', 'border-0', 'rounded-0');
+  const feeds = document.getElementsByClassName('feeds').item(0);
+  feeds.innerHTML = '';
+  const feedsBorder = document.createElement('div');
+  feedsBorder.classList.add('card', 'border-0');
+  const feedsBody = document.createElement('div');
+  feedsBody.classList.add('card-body');
+  const feedsTitle = document.createElement('h2');
+  feedsTitle.classList.add('card-title', 'h4');
+  feedsTitle.innerHTML = 'Фиды';
+  const feedsUl = document.createElement('ul');
+  feedsUl.classList.add('list-group', 'border-0', 'rounded-0');
 
-    const posts = document.getElementsByClassName('posts').item(0);
-    posts.innerHTML = '';
-    const postsBorder = document.createElement('div');
-    postsBorder.classList.add('card', 'border-0');
-    const postsBody = document.createElement('div');
-    postsBody.classList.add('card-body');
-    const postsTitle = document.createElement('h2');
-    postsTitle.classList.add('card-title', 'h4');
-    postsTitle.innerHTML = 'Посты';
-    const postsUl = document.createElement('ul');
-    postsUl.classList.add('list-group', 'border-0', 'rounded-0');
+  const posts = document.getElementsByClassName('posts').item(0);
+  posts.innerHTML = '';
+  const postsBorder = document.createElement('div');
+  postsBorder.classList.add('card', 'border-0');
+  const postsBody = document.createElement('div');
+  postsBody.classList.add('card-body');
+  const postsTitle = document.createElement('h2');
+  postsTitle.classList.add('card-title', 'h4');
+  postsTitle.innerHTML = 'Посты';
+  const postsUl = document.createElement('ul');
+  postsUl.classList.add('list-group', 'border-0', 'rounded-0');
 
-    feedsBody.append(feedsTitle);
-    feedsBorder.append(feedsBody);
-    postsBody.append(postsTitle);
-    postsBorder.append(postsBody);
+  feedsBody.append(feedsTitle);
+  feedsBorder.append(feedsBody);
+  postsBody.append(postsTitle);
+  postsBorder.append(postsBody);
 
-    state.feeds.forEach((element) => {
-        const feedLi = document.createElement('li');
-        feedLi.classList.add('list-group-item', 'border-0', 'border-end-0');
-        const h3 = document.createElement('h3');
-        h3.classList.add('h6', 'm-0');
-        h3.innerHTML = element.title;
-        const p = document.createElement('p');
-        p.classList.add('m-0', 'small', 'text-black-50');
-        p.innerHTML = element.description;
-        feedLi.append(h3, p);
-        feedsUl.append(feedLi);
-        element.items.forEach((item) => {
-            const postLi = document.createElement('li');
-            postLi.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
-            const a = document.createElement('a');
-            a.classList.add('fw-bold');
-            a.setAttribute('href', item.link);
-            a.setAttribute('target', '_blank');
-            a.setAttribute('rel', 'noopener noreferrer');
-            a.innerHTML = item.title;
-            postLi.append(a);
-            postsUl.append(postLi);
-        });
+  state.feeds.forEach((element) => {
+    const feedLi = document.createElement('li');
+    feedLi.classList.add('list-group-item', 'border-0', 'border-end-0');
+    const h3 = document.createElement('h3');
+    h3.classList.add('h6', 'm-0');
+    h3.innerHTML = element.title;
+    const p = document.createElement('p');
+    p.classList.add('m-0', 'small', 'text-black-50');
+    p.innerHTML = element.description;
+    feedLi.append(h3, p);
+    feedsUl.append(feedLi);
+    element.items.forEach((item) => {
+      const postLi = document.createElement('li');
+      postLi.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+      const a = document.createElement('a');
+      const fwClass = state.viewedUrls.includes(item.link) ? 'fw-normal' : 'fw-bold';
+      a.classList.add(fwClass);
+      a.setAttribute('href', item.link);
+      a.setAttribute('target', '_blank');
+      a.setAttribute('rel', 'noopener noreferrer');
+      a.setAttribute('data-id', item.dataId);
+      a.innerHTML = item.title;
+      const button = document.createElement('button');
+      button.setAttribute('type', 'button');
+      button.setAttribute('data-bs-toggle', 'modal');
+      button.setAttribute('data-id', item.dataId);
+      button.setAttribute('data-bs-target', '#modal');
+      button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+      button.innerHTML = 'Просмотр';
+      postLi.append(a);
+      postLi.append(button);
+      postsUl.append(postLi);
     });
+  });
 
-    feedsBorder.append(feedsUl);
-    feeds.append(feedsBorder);
+  feedsBorder.append(feedsUl);
+  feeds.append(feedsBorder);
 
-    postsBorder.append(postsUl);
-    posts.append(postsBorder);
+  postsBorder.append(postsUl);
+  posts.append(postsBorder);
+
+  if (Object.keys(state.rssForm.modalContent).length !== 0) {
+    showModal(state);
+  }
 };
+
+function watchState(state) {
+  return onChange(state, () => {
+    render(state);
+  });
+}
+
+export default watchState;
